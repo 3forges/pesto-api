84ab2f74009a2b5427131f150eb4e9c44971fd3c
Jean-Baptiste Lasselle <jean.baptiste.lasselle@gmail.com>
2023-10-16 03:23:52 +0200

    doc: openapi.json is generated, then widdershins md, and i just added scripts to generate the slate static website into [docs_dist]

--- pesto-api/src/main.ts
+++ pesto-api/src/main.ts
@@ -39,10 +39,10 @@ async function bootstrap() {
    * https://docs.nestjs.com/openapi/introduction
    */
   const config = new DocumentBuilder()
-    .setTitle('Cats example')
-    .setDescription('The cats API description')
+    .setTitle('Pesto Example | PestoContentType Example')
+    .setDescription('The Pesto API description')
     .setVersion('1.0')
-    .addTag('cats')
+    .addTag('pesto')
     .build();
   const document = SwaggerModule.createDocument(app, config);
   SwaggerModule.setup('api', app, document);
