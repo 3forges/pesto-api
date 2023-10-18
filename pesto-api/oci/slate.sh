fd5b758294fd1fbfb9d58cf02fe295512903c084
Jean-Baptiste Lasselle <jean.baptiste.lasselle@gmail.com>
2023-10-18 22:26:01 +0200

    doc: added slate circleci pipeline job

--- pesto-api/oci/slate.sh
+++ pesto-api/oci/slate.sh
@@ -4,7 +4,7 @@
 #  https://github.com/slatedocs/slate/wiki/Using-Slate-in-Docker#getting-started
 # --
 # 
-export DOCS_DIST=./docs_dist/
+export DOCS_DIST="$(pwd)/docs_dist"
 export WIDDERSHINS_MD=${WIDDERSHINS_MD:-"./pesto.widdershins.md"}
 export SLATE_VERSION="2.13.1"
 
@@ -20,9 +20,9 @@ echo "# --- # --- "
 echo "# --- # --- # --- "
 echo "# --- # --- # --- # --- "
 
-git clone https://github.com/slatedocs/slate ${DOCS_DIST}
+git clone https://github.com/slatedocs/slate ${DOCS_DIST}/
 
-cd ${DOCS_DIST}
+cd ${DOCS_DIST}/
 
 git checkout ${SLATE_VERSION}
 
@@ -40,8 +40,25 @@ fi;
 
 docker pull slatedocs/slate
 
-docker run --rm --name slate -v $(pwd)/build:/srv/slate/build -v $(pwd)/source:/srv/slate/source slatedocs/slate build
+# docker run --rm --name slate -v $(pwd)/build:/srv/slate/build -v $(pwd)/source:/srv/slate/source slatedocs/slate build
+# -v $(pwd)/build:/srv/slate/build -v $(pwd)/source:/srv/slate/source
+# ENTRYPOINT ["/srv/slate/slate.sh"]
+# CMD ["build"]
 
+echo " SLATE DOCKER CONTAINER STARTING..."
+docker run --name slate -itd --restart always stdin_open slate slatedocs/slate /bin/sh
+echo " SLATE DOCKER CONTAINER STARTED."
+docker cp ${DOCS_DIST}/source/index.html.md slate:/srv/slate/source/index.html.md
+echo " index.html.md copied into SLATE DOCKER CONTAINER: COMPLETED."
+docker exec -it slate sh -c "/srv/slate/slate.sh build"
+echo " SLATE BUILD IN DOCKER CONTAINER: COMPLETED."
+mkdir -p ./build/
+docker cp slate:/srv/slate/build/. ./build/
+echo " SLATE BUILD ASSETS COPIED FROM DOCKER CONTAINER, TO DOCKER HOST: COMPLETED."
+docker stop slate && docker rm slate
+echo " SLATE DOCKER CONTAINER PRUNED."
+# docker cp src/. container_id:/target
+# docker cp container_id:/src/. target
 echo "# --- "
 echo "# --- # --- "
 echo "# --- # --- # --- "
