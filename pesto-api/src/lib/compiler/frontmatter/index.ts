b3eff24f8402f43090bf6264bd484c6e5e5731f0
Jean-Baptiste-Lasselle <jean.baptiste.lasselle.pokus@gmail.com>
2024-08-22 00:09:54 +0200

    a few important notes about how to validate a frontmatter against a typescript interface

--- pesto-api/src/lib/compiler/frontmatter/index.ts
+++ pesto-api/src/lib/compiler/frontmatter/index.ts
@@ -1,4 +1,17 @@
 /* eslint-disable prettier/prettier */
+/**
+ * The question is: How can I validate a frontmatter 
+ * againt a typescript interface?
+ * Ts interface => JSON Schema
+ * Yaml => JSON 
+ * JSON <=> JSON Schema
+ * 
+ * In this code generating JSON Schema :
+ * https://github.com/3forges/pesto-api/issues/10#issuecomment-1903783684
+ * I am sure i can find in Typescript Compiler API or ts-morph 
+ * a way to provide getRootNodes from a ttext instead of a file.
+ * 
+ */
 export class Converter {
   /**
    * 
